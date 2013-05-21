b9702de144eac13a63a909a59d0cabebd9b7cf14
John Gebbie <john.gebbie@appliedor.com>
2013-05-21 10:08:33 -0700

    skip words with abbreviations, make sure all words are lowercase print out additional password with uppercase and punctuation

--- passgen.m
+++ passgen.m
@@ -48,26 +48,69 @@ catch e
 end
 fclose(fid); fid = -1;
 
-% filter words
+% filter out words that do not meet certain criteria
 shortlist = {};
 for n = 1:length(all)
-    if min_word_len <= length(all{n}) && length(all{n}) <= max_word_len;
-        shortlist{end+1} = all{n}; %#ok
+    word = all{n};
+    
+    % skip words that are not the right length
+    if length(word) < min_word_len
+        continue;
     end
+    if length(word) > max_word_len
+        continue;
+    end
+    
+    % skip any words that have punctuation
+    if any(~isletter(word))
+        continue;
+    end
+    
+    % make lowercase
+    word = lower(word);
+    
+    % append to the shortlist of words
+    shortlist{end+1} = word; %#ok
 end
 
 % build password
 password = '';
-for n = 1:Nwords
+n = 1;
+while n <= Nwords
+    word = shortlist{randi(length(shortlist))};
+    
     if n > 1;
         password = [ password ' ' ]; %#ok
     end
-    password = [ password shortlist{randi(length(shortlist))} ]; %#ok
+    
+    % append
+    password = [ password word ]; %#ok
+    
+    % increment
+    n = n+1;
 end
 
 % compute bits of entropy
 entropy_bits = log(length(shortlist))/log(2)*Nwords;
 
 % print it out
+fprintf('%s (entropy = %.1f bits)\n', password, entropy_bits);
+
+% take out the spaces
+password(password == ' ') = [];
+fprintf('%s (no space)\n', password);
+
+% add uppercase, punctuation, and number
+password(1) = upper(password(1));           % capitalize first char
+rand_num = int2str(randi(10)-1);
+punct_chars = '!@#$%^&*;,./';
+rand_punct = punct_chars(randi(length(punct_chars)));
+password = [password, rand_num, rand_punct];
+
+% compute bits of entropy
+entropy_bits = entropy_bits + ...
+    log(10)/log(2) + log(length(punct_chars))/log(2);
+
+% print out modified password
 fprintf('%s (entropy = %.1f bits)\n', password, entropy_bits);
 
